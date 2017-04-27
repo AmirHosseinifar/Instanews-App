668084d9ac0f99ce078cfbb54f68c44ea2db9bae
AmirHosseinifar <hosseinifar.amir@gmail.com>
2017-04-27 10:59:07 -0700

    Instanews-App

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(function () {
-console.log("this is jquery......fd")
+$(function (){
+  $('#select').on(
+    'change', function (){
+      var url = "https://api.nytimes.com/svc/topstories/v2/politics.json";
+url += '?' + $.param({
+  'api-key': "d84a476d118d4f00a74b79e6bf7001d7"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
+
+    
+  )
 });
