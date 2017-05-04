a0d317e0b374a5fb6aa8a3a5e55a449dc0d16de5
AmirHosseinifar <hosseinifar.amir@gmail.com>
2017-05-04 09:24:43 -0700

    Instanews-App

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,35 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync'),
     eslint = require("gulp-eslint");
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'), 
+    cssnano = require('gulp-cssnano'),
+    prettyerror = require('gulp-prettyerror');
 
-gulp.task("scripts",['lint'], function(){
-   gulp.src("./js/*.js")
-  .pipe(uglify())
-  .pipe(rename({ extname: ".min.js"}))
-  .pipe(gulp.dest("./build/js"))
+gulp.task('sass', function(){
+   gulp.src('./scss/style.scss')
+    .pipe(prettyerror())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'))
+    
 });
 
-gulp.task('lint', function() {
-  return gulp.src(['./js/*.js','!node_modules/**'])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError());
+
+gulp.task('scripts',['lint'], function(){
+   gulp.src('./js/*.js')
+  .pipe(uglify())
+  .pipe(rename({ extname: '.min.js'}))
+  .pipe(gulp.dest('./build/js'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch("./js/*.js", ['scripts']);
+  gulp.watch('./scss/*.scss', ['sass']);
 });
 
 gulp.task('browser-sync', function(){
@@ -25,11 +41,16 @@ gulp.task('browser-sync', function(){
     }
   });
 
-  gulp.watch(['./build/js/*.js', './style.css']).on('change', browserSync.reload)
+  gulp.watch(['./build/js/*.js', './build/css/*.css']).on('change', browserSync.reload)
 });
 
-gulp.task('watch', function(){
-  gulp.watch("./js/*.js", ['scripts'])
+
+
+gulp.task('lint', function() {
+  return gulp.src(['./js/*.js','!node_modules/**'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['watch', 'browser-sync']);
--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
-$(function (){
-  $('#select').on(
-    'change', function (){
-      var url = "https://api.nytimes.com/svc/topstories/v2/politics.json";
-url += '?' + $.param({
-  'api-key': "d84a476d118d4f00a74b79e6bf7001d7"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+$(function () {
 
-    
-  )
+  $('#select').on('change', function (event) {
+    event.preventDefault();
+    var selected = this.value;
+    $(".news").empty();
+    $('.loading').show();
+    var story="";
+    var count=0;
+    var url = 'https://api.nytimes.com/svc/topstories/v2/'+selected+'.json';
+
+    url += '?' + $.param({
+      'api-key': 'd84a476d118d4f00a74b79e6bf7001d7'
+    });
+
+    $.ajax({
+      url: url,
+      method: 'GET'
+    }).done(function (data) {
+      
+      $.each(data.results, function(index, value){
+       if( value.multimedia.length >=5 && count<12 && value.multimedia[4].width >= 2048){story += '<div class=story-part>'<a href="'+ value.url +'" target="_blank"><img src="'+value.multimedia[4].url+'" class="story-image"><div class="abstract-container"><p class="story-abstract">'+value.abstract+'</p></div></a></div>';
+       count ++;
+}
+      });
+      $('.news').append(story)
+
+     }).fail(function (err) {
+      $('.news').append('<p>Sorry, nothing was found.</p>');
+    });
+
+  
+
+  });
 });
